62c7570143a35592bdd07e0b7cbfdf9915c3a725
yeshwant <yeshwantreddyanugu@gmail.com>
2025-05-24 12:26:41 +0530

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/mandir-seva-connect",
+   base: '/mandir-seva-connect/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
